65236b0ca8606ddc86bae02fb92803ece0262fa3
Joy <rlfmalehd@gmail.com>
2019-11-16 00:31:09 +0900

    init
    set default project

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sc = new Solution();
+        sc.solution(args);
+    }
+
+    private static class Solution {
+
+        public Solution() {
+
+        }
+
+        public int[] solution(String[] args) {
+            int[] answer = {};
+            return answer;
+        }
+
+    }
+
+}
