3017fb99e8a8210196c3b3e6678591bead4aa915
Joy <rlfmalehd@gmail.com>
2019-11-16 00:01:51 +0900

    전화번호 목록3
    실행시간 단축 시도

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,15 +13,19 @@ public class Main {
 
     public static boolean solution(String[] phone_book) {
         Map<String, Integer> phoneBook = new HashMap<>();
+        int minimuxLength = 20;
 
         for (String num : phone_book) {
             phoneBook.put(num, 1);
+            if(num.length() < minimuxLength){
+                minimuxLength = num.length();
+            }
         }
 
         Arrays.sort(phone_book, Comparator.comparingInt(String::length).reversed());
 
         for (String num : phone_book) {
-            for (int i = num.length() - 1; i > 0; i--) {
+            for (int i = num.length() - 1; i >= minimuxLength; i--) {
                 if (phoneBook.getOrDefault(num.substring(0, i), 0) == 1) {
                     return false;
                 }
