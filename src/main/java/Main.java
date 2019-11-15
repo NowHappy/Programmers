7d22f33f39bd1b04b68cdf6c2efa41574e47505d
Joy <rlfmalehd@gmail.com>
2019-11-16 00:08:38 +0900

    전화번호 목록4
    해쉬 사용하지 않는 방법  - 젤 빠름

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,26 +10,17 @@ public class Main {
     }
 
     public static boolean solution(String[] phone_book) {
-        Map<String, Integer> phoneBook = new HashMap<>();
-        int minimuxLength = 20;
-        //가능한 조합을 모두 저장한다 단, 자기 자신은 저장하지 않는다. 진부분 집합만 저장
-        for (String num : phone_book) {
-            if (num.length() < minimuxLength) {
-                minimuxLength = num.length();
-            }
-            for (int i = num.length() - 1; i >= minimuxLength; i--) {
-                phoneBook.put(num.substring(0, i), 1);
+        for(int i=0; i<phone_book.length; i++){
+            for(int j=0; j<phone_book.length; j++){
+                if(i != j){
+                    if(phone_book[j].startsWith(phone_book[i])){
+
+                        return false;
+                    }
+                }
+
             }
         }
-
-        // "전체 번호로"  검색해서 존재하면 false 아니면 true
-        for (String num : phone_book) {
-            if (phoneBook.getOrDefault(num, 0) == 1) {
-                return false;
-            }
-        }
-
-        System.out.println(phoneBook);
 
         return true;
     }
