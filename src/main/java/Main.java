b03366ca6606ee85d05116d276a93484524f411f
Joy <rlfmalehd@gmail.com>
2019-11-17 04:28:43 +0900

    위장1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        solution();
+        String[][] clothes = new String[][] {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(solution(clothes));
     }
 
-    private static void solution() {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> clotheMap = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            clotheMap.put(clothe[1], clotheMap.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        for (int num : clotheMap.values()) {
+            answer *= (num+1);
+        }
+
+        return answer-1;
 
     }
+
+
 
 }
