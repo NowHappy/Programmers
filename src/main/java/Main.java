057386c964d59910c98f4c17330a41cd5d036b28
Joy <rlfmalehd@gmail.com>
2019-11-17 15:09:05 +0900

    기능개발1
    스택/큐 > 기능개발
    https://www.welcomekakao.com/learn/courses/30/lessons/42586

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,58 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        solution();
+        Solution ex = new Solution();
+        int[] progresses = new int[]{93, 30, 55};
+        int[] speeds = new int[]{1, 30, 5};
+        System.out.print(Arrays.toString(ex.solution(progresses, speeds)));
     }
 
-    private static void solution() {
+    static class Solution {
+        public Solution() {
 
+        }
+
+        public int[] solution(int[] progresses, int[] speeds) {
+            boolean canDeploy = false;
+            int curPoint = 0;
+            int[] neededDays = new int[progresses.length];
+            for (int progress : progresses) {
+                while (progress < 100) {
+                    progress += speeds[curPoint];
+                    neededDays[curPoint]++;
+                }
+                curPoint++;
+            }
+
+            curPoint = 0;
+            int deployDate = neededDays[curPoint];
+            Map<Integer, Integer> num = new HashMap<>();
+            int days = 0;
+            for(int i = 0; i < progresses.length; i++){
+                if(neededDays[i] > deployDate){
+                    days++;
+                    deployDate = neededDays[i];
+                }
+                num.put(days, num.getOrDefault(days,0) + 1);
+            }
+
+            List<Integer> answer = new ArrayList<>();
+            for (int n : num.values()) {
+                if (n != 0) {
+                    answer.add(n);
+                }
+            }
+
+            int[] result = new int[answer.size()];
+            for (int i = 0; i < answer.size(); i++) {
+                result[i] = answer.get(i);
+            }
+
+            return result;
+        }
     }
+
 
 }
