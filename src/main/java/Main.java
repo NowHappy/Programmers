92a9d952176d49085a8cb90d7bbba4b67481221f
Joy <rlfmalehd@gmail.com>
2019-11-17 19:26:40 +0900

    프린터1
    스택/큐 > 프린터https://programmers.co.kr/learn/courses/30/lessons/42587

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution sc = new Solution();
-        sc.solution(args);
+        int[] priorities = new int[] {1, 1, 9, 1, 1, 1};
+        int location = 0;
+        System.out.println(sc.solution(priorities, location));
     }
 
     private static class Solution {
@@ -11,11 +18,66 @@ public class Main {
 
         }
 
-        public int[] solution(String[] args) {
-            int[] answer = {};
+        public int solution(int[] priorities, int location) {
+            int answer = 0;
+            List<Document> docs = new ArrayList<>();
+            Deque<Document> documentDeque = new ArrayDeque<>();
+
+            for (int i = 0; i < priorities.length; i++) {
+                Document doc = new Document(priorities[i], i);
+                docs.add(doc);
+                documentDeque.addLast(doc);
+            }
+
+            int count = 1;
+            while (!documentDeque.isEmpty()) {
+                Document doc = documentDeque.pop();
+                boolean isBiggerExist = false;
+                for (Document document : docs) {
+                    if (doc.getPriority() < document.getPriority()) {
+                        isBiggerExist = true;
+                        break;
+                    }
+                }
+                if (!isBiggerExist) {
+                    docs.remove(doc);
+                    if (doc.getLocation() == location) {
+                        answer = count;
+                    }
+                    count++;
+                } else {
+                    documentDeque.addLast(doc);
+                }
+            }
             return answer;
         }
 
+    }
+
+    static class Document {
+        private int priority;
+        private int location;
+
+        public Document(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+
+        public int getLocation() {
+            return location;
+        }
+
+        public void setLocation(int location) {
+            this.location = location;
+        }
     }
 
 }
