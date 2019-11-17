786f72b0df8bb4fb567e3ef57da18c197591613e
Joy <rlfmalehd@gmail.com>
2019-11-17 19:58:35 +0900

    탑1
    스택/큐 > 탑
    https://programmers.co.kr/learn/courses/30/lessons/42588

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution sc = new Solution();
-        sc.solution(args);
+        int[] heights = new int[] {3, 9, 9, 3, 5, 7, 2};
+        System.out.println(Arrays.toString(sc.solution(heights)));
     }
 
     private static class Solution {
@@ -11,9 +16,44 @@ public class Main {
 
         }
 
-        public int[] solution(String[] args) {
-            int[] answer = {};
+        public int[] solution(int[] heights) {
+            int[] answer = new int[heights.length];
+            List<Tower> towers = new ArrayList<>();
+            for (int i = 0; i < heights.length; i++) {
+                Tower tower = new Tower(i + 1, heights[i]);
+                towers.add(tower);
+            }
+            //towers.forEach(tower -> System.out.print(tower.getOrder() + " "));
+
+            for (int i = heights.length - 1; i >= 0; i--) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (towers.get(j).getHeight() > towers.get(i).getHeight()) {
+                        answer[i] = towers.get(j).getOrder();
+                        break;
+                    }
+                }
+            }
+
             return answer;
+        }
+
+        public static class Tower {
+            private int order;
+            private int height;
+
+            public Tower(int order, int height) {
+                this.order = order;
+                this.height = height;
+            }
+
+            public int getOrder() {
+                return order;
+            }
+
+            public int getHeight() {
+                return height;
+            }
+
         }
 
     }
