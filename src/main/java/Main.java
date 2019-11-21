23b36953e0cc7548a3574d29b49b7fd78012312f
Joy <rlfmalehd@gmail.com>
2019-11-21 19:17:11 +0900

    2017 카카오 코딩 테스트 문제 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution sc = new Solution();
-        sc.solution(args);
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        //#####, # # #, ### #, #  ##, #####
+        System.out.println(Arrays.toString(sc.solution(n, arr1, arr2)));
     }
 
     private static class Solution {
@@ -11,8 +17,21 @@ public class Main {
 
         }
 
-        public int[] solution(String[] args) {
-            int[] answer = {};
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            int targetBit = 1;
+            String[] answer = new String[n];
+            String resultString = null;
+            for (int i = 0; i < n; i++) {
+                resultString = "";
+                targetBit = 1;
+                int arr = arr1[i] | arr2[i];
+                for (int j = 0; j < n; j++) {
+                    resultString = ((arr & targetBit) > 0 ? "#" : " ") + resultString;
+                    targetBit = targetBit << 1;
+                }
+                answer[i] = resultString;
+            }
+
             return answer;
         }
 
