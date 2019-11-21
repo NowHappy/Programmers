e199ffad1c16d0aa6d88031d708a537cfda4afec
Joy <rlfmalehd@gmail.com>
2019-11-21 20:37:28 +0900

    문제3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     //Line Coding Test Problem 3
 
-    public static void main(String[] args) {
-        Solution sc = new Solution();
-        sc.solution(args);
-    }
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int target = Integer.valueOf(br.readLine().toString());
+        List<Integer> numbers = Arrays.stream(input).map(num -> Integer.valueOf(num)).collect(Collectors.toList());
 
-    private static class Solution {
+        numbers.sort(Integer::compareTo);
 
-        public Solution() {
+        List<Integer> compares = new ArrayList<>(numbers);
 
-        }
-
-        public int[] solution(String[] args) {
-            int[] answer = {};
-            return answer;
+        for (int num : numbers) {
+            compares.remove(compares.indexOf(num));
+            int rest = target - num;
+            if (compares.contains(rest) && num != rest) {
+                System.out.println(num + " " + rest);
+                return;
+            }
         }
 
     }
