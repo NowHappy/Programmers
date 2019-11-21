14fb7cbb1ddccc1c8d7988113338435dc0be1b50
Joy <rlfmalehd@gmail.com>
2019-11-21 21:53:08 +0900

    문제5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Main {
     //Line Coding Test Problem 5
 
-    public static void main(String[] args) {
-        Solution sc = new Solution();
-        sc.solution(args);
-    }
-
-    private static class Solution {
-
-        public Solution() {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int windowSize = Integer.valueOf(br.readLine().toString());
+        Deque<Integer> deque = new LinkedBlockingDeque(windowSize);
+        int num, max = 0, prev, count = 0;
+        while (br.ready()) {
+            num = Integer.valueOf(br.readLine().toString());
+            if (deque.size() >= windowSize) {
+                prev = deque.pop();
+                if (prev >= max) {
+                    max = deque.stream().mapToInt(n -> n)
+                            .max().getAsInt();
+                }
+            }
+            deque.add(num);
+            if (num > max) {
+                max = num;
+            }
+            count++;
+            if(count >= windowSize){
+                System.out.println(max);
+            }
         }
-
-        public int[] solution(String[] args) {
-            int[] answer = {};
-            return answer;
-        }
-
     }
 
 }
