a3774cb0e4ec72d5d21188f9925e89493d597c9c
Joy <rlfmalehd@gmail.com>
2019-11-21 20:16:29 +0900

    문제 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Main {
     //Line Coding Test Problem 2
 
-    public static void main(String[] args) {
-        Solution sc = new Solution();
-        sc.solution(args);
-    }
-
-    private static class Solution {
-
-        public Solution() {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputInfo = br.readLine().split(" ");
+        int orderSize = Integer.valueOf(inputInfo[0]);
+        int quereSize = Integer.valueOf(inputInfo[1]);
+        Deque<String> deque = new LinkedBlockingDeque(quereSize);
+        String[] inputString;
+        for (int i = 0; i < orderSize; i++) {
+            inputString = br.readLine().split(" ");
+            if (inputString[0].equals("OFFER")) {
+                System.out.println(deque.offer(inputString[1]));
+            }
+            if (inputString[0].equals("TAKE")) {
+                System.out.println(deque.pop());
+            }
+            if (inputString[0].equals("SIZE")) {
+                System.out.println(deque.size());
+            }
         }
-
-        public int[] solution(String[] args) {
-            int[] answer = {};
-            return answer;
-        }
-
     }
 
 }
